3800017d0fd6e681fc94b1a37d9c3aef3251a694
AlexLog22 <logvin.alex2005@gmail.com>
2024-06-07 09:55:30 +0300

    Second commit

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int a = 10;
+    float y = 15.3;
+    if (a > y){
+        printf("Hello, World!\n");
+    }
+    else
+        printf("No\n");
     return 0;
 }
