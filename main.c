06f71c4397b37a0a7818ecccfb71579fedaa7993
AlexLog22 <logvin.alex2005@gmail.com>
2024-06-07 11:17:21 +0300

    second exam commit

--- main.c
+++ main.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     int n;
-    printf("Enter number of elements: ");
+    printf("Enter the number of elements: ");
     scanf("%d", &n);
 
-    int a[n];
-
-    printf("Wirte elements ");
-    for(int i = 0; i < n; i++){
+    int *a = (int *)malloc(n * sizeof(int));
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++)
         scanf("%d", &a[i]);
+
+    int count = 0;
+    int *unique = (int *)malloc(n * sizeof(int));
+    int *nonUnique = (int *)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++){
+        int isUnique = 1;
+        for (int j = i+1; j < n; j++){
+            if (a[i] == a[j]) {
+                isUnique = 0;
+                break;
+            }
+        }
+        if (!isUnique)
+            nonUnique[count++] = a[i];
+        else
+            unique[i-count] = a[i];
     }
 
-    int non-unque = 0;
-    int unque = 0;
+    printf("There are %d non-unique elements:\n", count);
+    for (int i = 0; i < count; i++)
+        printf("%d ", nonUnique[i]);
 
-
-
+    return 0;
 }
